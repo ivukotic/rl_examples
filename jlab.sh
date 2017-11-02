fc7cbd38247a6aa06abe8219d4b8939b5744f7d0
Ilija Vukotic <ivukotic@uchicago.edu>
2017-11-02 15:19:51 -0500

    Update jlab.sh
    fix

--- jlab.sh
+++ jlab.sh
@@ -1,5 +1,5 @@
 mkdir /root/.jupyter/
-wget https://raw.githubusercontent.com/ivukotic/ML_platform_tests/master/jupyter_notebook_config.py
+wget https://raw.githubusercontent.com/ivukotic/rl_examples/master/jupyter_notebook_config.py
 mv jupyter_notebook_config.py /root/.jupyter/jupyter_notebook_config.py
 export SHELL=/bin/bash
 
