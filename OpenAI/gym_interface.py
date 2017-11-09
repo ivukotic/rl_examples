3dfab97fd5b3faa42a047648054dbc2cbea74796
ILIJAXPS\ivukotic <ivukotic@cern.ch>
2017-11-09 17:44:21 -0600

    star

--- OpenAI/gym_interface.py
+++ OpenAI/gym_interface.py
@@ -1,7 +1,21 @@
 import gym
-import numpy as np
-import matplotlib
+# import numpy as np
+# import matplotlib
 
 # print all the environments
-for e in gym.envs.registry.all():
-    print(e)
+# for e in gym.envs.registry.all():
+#     print(e)
+
+env = gym.make('CartPole-v0')
+#print(dir(env))
+
+# env.__dict__
+
+observation = env.reset()
+for t in range(500):
+    env.render()
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+    print ("action:", action, "\t observation:", observation, '\treward:',reward, '\tdone:', done)
+    if done:
+        break
