d962d54edc8dd5f2b73cc12f7a05efb65a57daaa
ILIJAXPS\ivukotic <ivukotic@cern.ch>
2017-11-14 20:12:59 -0600

    working env. simple actor.

--- gym-link/gym_link/envs/link_env.py
+++ gym-link/gym_link/envs/link_env.py
@@ -1,15 +1,19 @@
 """
-One network link environment. 
+One network link environment.
 Link has changing base load.
 Actions: start 0 to 4 more transfers
-Reward: percentage of free rate used. Gets negative if link fully saturated   
+Reward: percentage of free rate used. Gets negative if link fully saturated
 Files sizes are normally distributed (absolute values).
 """
 
 import math
+from collections import deque
+
 import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
+from gym.envs.classic_control import rendering
+
 import numpy as np
 
 
@@ -31,18 +35,18 @@ class LinkEnv(gym.Env):
         self.dt = 1  # seconds
         #  key: int, start: int, size: int [bytes], transfered: int[bytes]
         self.transfers = {}
-        self.current_base_rate = self.max_link_rate * 0.5 * np.random.ranf()
-        self.step = 0
+        self.current_base_rate = int(self.max_link_rate * 0.5 * np.random.ranf())
+        self.tstep = 0
         self.ntransfers = 0
-        self.state = None
         self.viewer = None
+        self.h_base = deque(maxlen=600)
+        self.h_added = deque(maxlen=600)
         self._seed()
 
         # obesrvation space reports only on files transfered: rate and how many steps ago it started.
         self.observation_space = spaces.Box(
-            low=np.array([0, 0]),
-            high=np.array([np.finfo(np.float32).max, np.iinfo(np.int32).max]),
-            shape=(1)
+            low=np.array([0.0, 0]),
+            high=np.array([np.finfo(np.float32).max, np.iinfo(np.int32).max])
         )
         self.action_space = spaces.Discrete(4)
 
@@ -53,21 +57,23 @@ class LinkEnv(gym.Env):
     def _step(self, action):
 
         # add transfers if asked for
-        for i in action:
+        for i in range(action):
             file_size = int(math.fabs(self.file_size_mean + np.random.standard_normal() * self.file_size_sigma))
-            self.transfers[self.ntransfers] = [self.step, file_size, 0]
+            self.transfers[self.ntransfers] = [self.tstep, file_size, 0]
+            self.ntransfers += 1
 
         # find current base rate
-            self.current_base_rate += int(np.random.standard_normal(0, 8 * 1024 * 1024))
-            if self.current_base_rate > self.base_rate_max:
-                self.current_base_rate = self.base_rate_max
-            if self.current_base_rate < self.base_rate_min:
-                self.current_base_rate = self.base_rate_min
+        self.current_base_rate += int(np.random.standard_normal() * 8 * 1024 * 1024)
+        if self.current_base_rate > self.base_rate_max:
+            self.current_base_rate = self.base_rate_max
+        if self.current_base_rate < self.base_rate_min:
+            self.current_base_rate = self.base_rate_min
 
         # find used rate if all the ongoing transfers would be at maximal rate
         active_transfers = 0
         for t in self.transfers.values():
-            if self.step < self.handshake_duration + t[0]:
+            # print(t)
+            if self.tstep < self.handshake_duration + t[0]:
                 continue
             active_transfers += 1
 
@@ -78,32 +84,46 @@ class LinkEnv(gym.Env):
 
         reward = max_rate / max_free_bandwidth * 100
         if reward > 100:
-            reward = -reward
+            reward = -reward + 100
 
-        if reward < -100:
+        episode_over = False
+        if (max_rate + self.current_base_rate) > 1.1 * self.max_link_rate:
             episode_over = True
-        else:
-            episode_over = False
 
-        current_rate_per_file = math.floor(max_free_bandwidth / active_transfers)
+        current_rate_per_file = 0
+        if active_transfers > 0:
+            current_rate_per_file = min(math.floor(max_free_bandwidth / active_transfers), self.max_rate_per_file)
 
         time_of_last_started_finished_transfer = 0
         rate_of_last_started_finished_transfer = 0
+        finished = 0
         for k, t in self.transfers.items():
-            if self.step < self.handshake_duration + t[0]:
+            if self.tstep < self.handshake_duration + t[0]:
                 continue
             t[2] += current_rate_per_file
             if t[2] > t[1]:  # it is finished
+                finished += 1
                 if t[0] > time_of_last_started_finished_transfer:  # last started from all finished
-                    rate_of_last_started_finished_transfer = t[1] / (self.step - t[0])
-                print("transfer ", k, "finished")
+                    rate_of_last_started_finished_transfer = t[1] / (self.tstep - t[0])
+                    time_of_last_started_finished_transfer = t[0]
 
         observation = (rate_of_last_started_finished_transfer, time_of_last_started_finished_transfer)
+        self.tstep += 1
 
-        return observation, reward, episode_over, {}
+        self.h_base.append(self.current_base_rate)
+        self.h_added.append(max_rate + self.current_base_rate)
+
+        return observation, reward, episode_over, {
+            "finished transfers": finished,
+            "active transfers": active_transfers,
+            "base rate [%]": int(self.current_base_rate / self.max_link_rate * 10000) / 100
+        }
 
     def _reset(self):
-        ...
+        self.tstep = 0
+        self.transfers = {}
+        self.ntransfers = 0
+        return np.array((0, 0))
 
     def _render(self, mode='human', close=False):
         if close:
@@ -112,44 +132,59 @@ class LinkEnv(gym.Env):
                 self.viewer = None
             return
 
-        screen_width = 600
-        screen_height = 400
+        screen_width = 640
+        screen_height = 480
 
-        # world_width = self.x_threshold * 2
-        # scale = screen_width / world_width
-        # carty = 100  # TOP OF CART
-        # polewidth = 10.0
-        # polelen = scale * 1.0
-        # cartwidth = 50.0
-        # cartheight = 30.0
+        scale = np.max(self.h_added) / 440
 
-        # if self.viewer is None:
-        #     from gym.envs.classic_control import rendering
-        #     self.viewer = rendering.Viewer(screen_width, screen_height)
+        bdata = []  # (screen_width - 20, 20)]  # first point in lower right corner
+        y = list(reversed(self.h_base))
+        for j, i in enumerate(y):
+            bdata.append((screen_width - 20 - j, 20 + int(i / scale)))
+        # bdata.append((screen_width - 20 - len(y), 20))
+
+        adata = []  # (screen_width - 20, 20)]
+        y = list(reversed(self.h_added))
+        for j, i in enumerate(y):
+            adata.append((screen_width - 20 - j, 20 + int(i / scale)))
+        # adata.append((screen_width - 20 - len(y), 20))
+        adata = adata[:self.tstep]
+        if self.viewer is None:
+            self.viewer = rendering.Viewer(screen_width, screen_height)
         #     l, r, t, b = -cartwidth / 2, cartwidth / 2, cartheight / 2, -cartheight / 2
         #     axleoffset = cartheight / 4.0
         #     cart = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
         #     self.carttrans = rendering.Transform()
         #     cart.add_attr(self.carttrans)
         #     self.viewer.add_geom(cart)
-        #     l, r, t, b = -polewidth / 2, polewidth / 2, polelen - polewidth / 2, -polewidth / 2
-        #     pole = rendering.FilledPolygon([(l, b), (l, t), (r, t), (r, b)])
-        #     pole.set_color(.8, .6, .4)
         #     self.poletrans = rendering.Transform(translation=(0, axleoffset))
         #     pole.add_attr(self.poletrans)
         #     pole.add_attr(self.carttrans)
-        #     self.viewer.add_geom(pole)
         #     self.axle = rendering.make_circle(polewidth / 2)
         #     self.axle.add_attr(self.poletrans)
         #     self.axle.add_attr(self.carttrans)
-        #     self.axle.set_color(.5, .5, .8)
-        #     self.viewer.add_geom(self.axle)
-        #     self.track = rendering.Line((0, carty), (screen_width, carty))
-        #     self.track.set_color(0, 0, 0)
-        #     self.viewer.add_geom(self.track)
+            self.xaxis = rendering.Line((20, 20), (screen_width - 20, 20))
+            self.xaxis.set_color(0, 0, 0)
+            self.yaxis = rendering.Line((20, 20), (20, screen_height - 20))
+            self.yaxis.set_color(0, 0, 0)
+            self.viewer.add_geom(self.xaxis)
+            self.viewer.add_geom(self.yaxis)
 
-        if self.state is None:
-            return None
+        adde = rendering.PolyLine(adata, False)
+        adde.set_color(.1, .6, .8)
+        self.viewer.add_onetime(adde)
+
+        base = rendering.PolyLine(bdata, False)
+        base.set_color(.8, .6, .4)
+        self.viewer.add_onetime(base)
+
+        max_line = self.max_link_rate / scale
+        ml = rendering.Line((20, max_line + 20), (screen_width - 20, max_line + 20))
+        ml.set_color(0.1, 0.9, .1)
+        self.viewer.add_onetime(ml)
+
+        # if self.state is None:
+        # return None
 
         # x = self.state
         # cartx = x[0] * scale + screen_width / 2.0  # MIDDLE OF CART
--- gym-link/train_link.py
+++ gym-link/train_link.py
@@ -1,24 +1,98 @@
 import gym
 import gym_link
 
-env = gym.make('Link-v0')
+import math
+import random
+import numpy as np
+from collections import deque
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import Adam
 
-print(env.action_space)
-print(env.observation_space)
-print(env.observation_space.low)
-print(env.observation_space.high)
 
-for i_episode in range(5):
-    observation = env.reset()
-    tot_reward = 0
-    for t in range(100):
-        # env.render()
-        # print(observation)
-        action = env.action_space.sample()
-        observation, reward, done, info = env.step(action)
-        print('obs:', observation, '\trew:', reward, '\tdone:', done)
-        tot_reward += reward
-        if done:
-            print("Episode finished after {} timesteps".format(t + 1))
-            print("total rewards", tot_reward)
-            break
+class LinkSolver():
+    def __init__(self, n_episodes=1000, gamma=1.0, epsilon=1.0, epsilon_min=0.01, epsilon_log_decay=0.995, lr=0.01, lr_decay=0.01, batch_size=64):
+        self.memory = deque(maxlen=100000)
+        self.env = gym.make('Link-v0')
+        self.gamma = gamma
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_log_decay
+        self.lr = lr
+        self.lr_decay = lr_decay
+        self.n_episodes = n_episodes
+        self.batch_size = batch_size
+
+        # Init model
+        self.model = Sequential()
+        self.model.add(Dense(24, input_dim=2, activation='tanh'))
+        self.model.add(Dense(48, activation='tanh'))
+        self.model.add(Dense(4, activation='linear'))
+        self.model.compile(loss='mse', optimizer=Adam(lr=self.lr, decay=self.lr_decay))
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def choose_action(self, state, epsilon):
+        return self.env.action_space.sample() if (np.random.random() <= epsilon) else np.argmax(self.model.predict(state))
+
+    def get_epsilon(self, t):
+        return max(self.epsilon_min, min(self.epsilon, 1.0 - math.log10((t + 1) * self.epsilon_decay)))
+
+    def preprocess_state(self, state):
+        return np.reshape(state, [1, 2])
+
+    def replay(self, batch_size):
+        x_batch, y_batch = [], []
+        minibatch = random.sample(self.memory, min(len(self.memory), batch_size))
+        for state, action, reward, next_state, done in minibatch:
+            y_target = self.model.predict(state)
+            if done:
+                y_target[0][action] = reward
+            else:
+                y_target[0][action] = reward + self.gamma * np.max(self.model.predict(next_state)[0])
+            x_batch.append(state[0])
+            y_batch.append(y_target[0])
+
+        self.model.fit(np.array(x_batch), np.array(y_batch), batch_size=len(x_batch), verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def run(self):
+        avg_reward = deque(maxlen=100)
+        avg_life = deque(maxlen=100)
+        for e in range(self.n_episodes):
+            state = self.preprocess_state(self.env.reset())
+            done = False
+            tot_reward = 0
+            steps = 0
+            while not done:
+                action = self.choose_action(state, self.get_epsilon(e))
+                next_state, reward, done, _ = self.env.step(action)
+                self.env.render()
+                next_state = self.preprocess_state(next_state)
+                self.remember(state, action, reward, next_state, done)
+                state = next_state
+                tot_reward += reward
+                steps += 1
+                # if steps % 200 == 0:
+                #    print('episode:', e, '\tsteps:', steps)
+
+            avg_reward.append(tot_reward)
+            avg_life.append(steps)
+
+            mean_reward = np.mean(avg_reward)
+            mean_life = np.mean(avg_life)
+
+            print('duration:{}    reward:{}'.format(steps, tot_reward))
+
+            if e % 100 == 0:
+                print('[Episode {}] - Mean survival time over last 100 episodes was {} s. Mean reward was {}.'.format(e, mean_life, mean_reward))
+
+            self.replay(self.batch_size)
+        return
+
+
+if __name__ == '__main__':
+    agent = LinkSolver()
+    agent.run()
